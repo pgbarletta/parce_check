752f7e2e76d419651fe377a4e3f68b752a1b2a6e
Patricio <pbarletta@gmail.com>
2023-01-02 13:23:56 +0100

    d11 restrained optimization scores.

---
+++ d11/restrained/get_scores.sh
@@ -0,0 +1,84 @@
+mkdir -p 0-B_AGWENTI-B_YASSYPA-B_AKIRNQLHGRGGKSDSPT/scoring
+mkdir -p 1-B_AGWENTI-B_YADSYPA-B_AKIRNQLH/scoring/
+mkdir -p 1-B_AGWENTI-B_YAFSYPA-B_AKIRNQLH/scoring/
+mkdir -p 1-B_AGWENTI-B_YAGSYPA-B_AKIRNQLH/scoring/
+mkdir -p 1-B_AGWENTI-B_YAQSYPA-B_AKIRNQLH/scoring/
+mkdir -p 2-B_AGWENTI-B_YAFSYFA-B_AKIRNQLH/scoring/
+mkdir -p 2-B_AGWENTI-B_YAFSYGA-B_AKIRNQLH/scoring/
+mkdir -p 2-B_AGWENTI-B_YAFSYHA-B_AKIRNQLH/scoring/
+mkdir -p 2-B_AGWENTI-B_YAFSYSA-B_AKIRNQLH/scoring/
+mkdir -p 3-B_AGWENTI-B_YAFSYGA-B_HKIRNQLH/scoring/
+mkdir -p 3-B_AGWENTI-B_YAFSYHA-B_GKIRNQLH/scoring/
+mkdir -p 3-B_AGWENTI-B_YAFSYHA-B_TKIRNQLH/scoring/
+mkdir -p 3-B_AGWENTI-B_YAFSYSA-B_YKIRNQLH/scoring/
+mkdir -p 4-B_AGWENTI-B_YAFSYHA-B_TKIRNQPH/scoring/
+mkdir -p 4-B_AGWENTI-B_YAFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 4-B_AGWENTI-B_YAFSYHA-B_TKIRNQSH/scoring/
+mkdir -p 4-B_AGWENTI-B_YAFSYHA-B_TKIRNQVH/scoring/
+mkdir -p 5-B_AGWENTI-B_YEFSYHA-B_TKIRNQVH/scoring/
+mkdir -p 5-B_AGWENTI-B_YPFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 5-B_AGWENTI-B_YQFSYHA-B_TKIRNQVH/scoring/
+mkdir -p 5-B_AGWENTI-B_YVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 6-B_AGWENTI-B_AVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 6-B_AGWENTI-B_QVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 6-B_AGWENTI-B_SVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 6-B_AGWENTI-B_WVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 7-B_AGWENTE-B_YVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 7-B_AGWENTL-B_YVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 7-B_AGWENTP-B_YVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 7-B_AGWENTR-B_YVFSYHA-B_TKIRNQRH/scoring/
+mkdir -p 8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRA/scoring/
+mkdir -p 8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRE/scoring/
+mkdir -p 8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRR/scoring/
+mkdir -p 8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRW/scoring/
+mkdir -p 9-B_AGWENTL-B_YVFEYHA-B_TKIRNQRE/scoring/
+mkdir -p 9-B_AGWENTL-B_YVFIYHA-B_TKIRNQRE/scoring/
+mkdir -p 9-B_AGWENTL-B_YVFNYHA-B_TKIRNQRW/scoring/
+mkdir -p 9-B_AGWENTL-B_YVFYYHA-B_TKIRNQRW/scoring/
+mkdir -p 10-B_AGWENTL-B_YVFIYHA-B_TKLRNQRE/scoring/
+mkdir -p 10-B_AGWENTL-B_YVFIYHA-B_TKNRNQRE/scoring/
+mkdir -p 10-B_AGWENTL-B_YVFIYHA-B_TKTRNQRE/scoring/
+mkdir -p 10-B_AGWENTL-B_YVFIYHA-B_TKWRNQRE/scoring/
+
+
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/0-B_AGWENTI-B_YASSYPA-B_AKIRNQLHGRGGKSDSPT/scoring/scores_*     ./0-B_AGWENTI-B_YASSYPA-B_AKIRNQLHGRGGKSDSPT/scoring
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/1-B_AGWENTI-B_YADSYPA-B_AKIRNQLH/scoring/scores_*               ./1-B_AGWENTI-B_YADSYPA-B_AKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/1-B_AGWENTI-B_YAFSYPA-B_AKIRNQLH/scoring/scores_*               ./1-B_AGWENTI-B_YAFSYPA-B_AKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/1-B_AGWENTI-B_YAGSYPA-B_AKIRNQLH/scoring/scores_*               ./1-B_AGWENTI-B_YAGSYPA-B_AKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/1-B_AGWENTI-B_YAQSYPA-B_AKIRNQLH/scoring/scores_*               ./1-B_AGWENTI-B_YAQSYPA-B_AKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/2-B_AGWENTI-B_YAFSYFA-B_AKIRNQLH/scoring/scores_*               ./2-B_AGWENTI-B_YAFSYFA-B_AKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/2-B_AGWENTI-B_YAFSYGA-B_AKIRNQLH/scoring/scores_*               ./2-B_AGWENTI-B_YAFSYGA-B_AKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/2-B_AGWENTI-B_YAFSYHA-B_AKIRNQLH/scoring/scores_*               ./2-B_AGWENTI-B_YAFSYHA-B_AKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/2-B_AGWENTI-B_YAFSYSA-B_AKIRNQLH/scoring/scores_*               ./2-B_AGWENTI-B_YAFSYSA-B_AKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/3-B_AGWENTI-B_YAFSYGA-B_HKIRNQLH/scoring/scores_*               ./3-B_AGWENTI-B_YAFSYGA-B_HKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/3-B_AGWENTI-B_YAFSYHA-B_GKIRNQLH/scoring/scores_*               ./3-B_AGWENTI-B_YAFSYHA-B_GKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/3-B_AGWENTI-B_YAFSYHA-B_TKIRNQLH/scoring/scores_*               ./3-B_AGWENTI-B_YAFSYHA-B_TKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/3-B_AGWENTI-B_YAFSYSA-B_YKIRNQLH/scoring/scores_*               ./3-B_AGWENTI-B_YAFSYSA-B_YKIRNQLH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/4-B_AGWENTI-B_YAFSYHA-B_TKIRNQPH/scoring/scores_*               ./4-B_AGWENTI-B_YAFSYHA-B_TKIRNQPH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/4-B_AGWENTI-B_YAFSYHA-B_TKIRNQRH/scoring/scores_*               ./4-B_AGWENTI-B_YAFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/4-B_AGWENTI-B_YAFSYHA-B_TKIRNQSH/scoring/scores_*               ./4-B_AGWENTI-B_YAFSYHA-B_TKIRNQSH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/4-B_AGWENTI-B_YAFSYHA-B_TKIRNQVH/scoring/scores_*               ./4-B_AGWENTI-B_YAFSYHA-B_TKIRNQVH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/5-B_AGWENTI-B_YEFSYHA-B_TKIRNQVH/scoring/scores_*               ./5-B_AGWENTI-B_YEFSYHA-B_TKIRNQVH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/5-B_AGWENTI-B_YPFSYHA-B_TKIRNQRH/scoring/scores_*               ./5-B_AGWENTI-B_YPFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/5-B_AGWENTI-B_YQFSYHA-B_TKIRNQVH/scoring/scores_*               ./5-B_AGWENTI-B_YQFSYHA-B_TKIRNQVH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/5-B_AGWENTI-B_YVFSYHA-B_TKIRNQRH/scoring/scores_*               ./5-B_AGWENTI-B_YVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/6-B_AGWENTI-B_AVFSYHA-B_TKIRNQRH/scoring/scores_*               ./6-B_AGWENTI-B_AVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/6-B_AGWENTI-B_QVFSYHA-B_TKIRNQRH/scoring/scores_*               ./6-B_AGWENTI-B_QVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/6-B_AGWENTI-B_SVFSYHA-B_TKIRNQRH/scoring/scores_*               ./6-B_AGWENTI-B_SVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/6-B_AGWENTI-B_WVFSYHA-B_TKIRNQRH/scoring/scores_*               ./6-B_AGWENTI-B_WVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/7-B_AGWENTE-B_YVFSYHA-B_TKIRNQRH/scoring/scores_*               ./7-B_AGWENTE-B_YVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/7-B_AGWENTL-B_YVFSYHA-B_TKIRNQRH/scoring/scores_*               ./7-B_AGWENTL-B_YVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/7-B_AGWENTP-B_YVFSYHA-B_TKIRNQRH/scoring/scores_*               ./7-B_AGWENTP-B_YVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/7-B_AGWENTR-B_YVFSYHA-B_TKIRNQRH/scoring/scores_*               ./7-B_AGWENTR-B_YVFSYHA-B_TKIRNQRH/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRA/scoring/scores_*               ./8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRA/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRE/scoring/scores_*               ./8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRE/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRR/scoring/scores_*               ./8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRR/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRW/scoring/scores_*               ./8-B_AGWENTL-B_YVFSYHA-B_TKIRNQRW/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/9-B_AGWENTL-B_YVFEYHA-B_TKIRNQRE/scoring/scores_*               ./9-B_AGWENTL-B_YVFEYHA-B_TKIRNQRE/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/9-B_AGWENTL-B_YVFIYHA-B_TKIRNQRE/scoring/scores_*               ./9-B_AGWENTL-B_YVFIYHA-B_TKIRNQRE/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/9-B_AGWENTL-B_YVFNYHA-B_TKIRNQRW/scoring/scores_*               ./9-B_AGWENTL-B_YVFNYHA-B_TKIRNQRW/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/9-B_AGWENTL-B_YVFYYHA-B_TKIRNQRW/scoring/scores_*               ./9-B_AGWENTL-B_YVFYYHA-B_TKIRNQRW/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/10-B_AGWENTL-B_YVFIYHA-B_TKLRNQRE/scoring/scores_*              ./10-B_AGWENTL-B_YVFIYHA-B_TKLRNQRE/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/10-B_AGWENTL-B_YVFIYHA-B_TKNRNQRE/scoring/scores_*              ./10-B_AGWENTL-B_YVFIYHA-B_TKNRNQRE/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/10-B_AGWENTL-B_YVFIYHA-B_TKTRNQRE/scoring/scores_*              ./10-B_AGWENTL-B_YVFIYHA-B_TKTRNQRE/scoring/
+rsync -rzvhu pbarlett@login.m100.cineca.it:/m100_work/AIRC_Fortun21/barletta/twist/restrained_optimization/d11/work_dir/10-B_AGWENTL-B_YVFIYHA-B_TKWRNQRE/scoring/scores_*              ./10-B_AGWENTL-B_YVFIYHA-B_TKWRNQRE/scoring/
